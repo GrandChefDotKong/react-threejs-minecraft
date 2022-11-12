e8f3f9ca96d7396ce43e9727a4eee510f57c68a9
Adrien Pauchet <grandchefdotkong@gmail.com>
2022-11-12 14:36:59 +0900

    camera && movement added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,8 +2,14 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon";
 import { useRef, useEffect } from 'react';
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard"
+
+const JUMP_FORCE = 4;
+const SPEED = 4;
 
 const Player = () => {
+  const { moveBackward, moveForward, moveRight, moveLeft, jump } = useKeyboard();
+
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1, type: 'Dynamic', position: [0, 2, 0]
@@ -23,13 +29,33 @@ const Player = () => {
   
 
   useFrame(() => {
-    camera.position.copy(
-      new Vector3(pos.current[0], pos.current[1], pos.current[2])
-    );
+    camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
 
-    api.velocity.set(0, 0, 0);
+		const direction = new Vector3()
+		const frontVector = new Vector3(
+			0,
+			0,
+			(moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+		)
 
-  })
+		const sideVector = new Vector3(
+			(moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+			0,
+			0,
+		)
+
+		direction
+			.subVectors(frontVector, sideVector)
+			.normalize()
+			.multiplyScalar(SPEED)
+			.applyEuler(camera.rotation)
+
+		api.velocity.set(direction.x, vel.current[1], direction.z)
+
+		if (jump && Math.abs(vel.current[1]) < 0.05) {
+			api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2])
+		}
+	})
 
   return (
     <mesh ref={ref}></mesh>
